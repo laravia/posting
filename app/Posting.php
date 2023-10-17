02b97cfb4d693469854151d7f41eadbe9594722a
manuel <code@leunam.art>
2023-10-17 18:59:59 +0200

    [feat] added getDashboardMetrics

--- app/Posting.php
+++ app/Posting.php
@@ -2,6 +2,20 @@
 
 namespace Laravia\Posting\App;
 
+use Laravia\Posting\App\Orchid\Screens\PostingScreen;
+
 class Posting
 {
+    public static function getDashboardMetrics($what)
+    {
+        try {
+            switch ($what) {
+                case 'postings':
+                    return data_get((new PostingScreen())->query(), 'metrics.postings.all');
+                    break;
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
 }
