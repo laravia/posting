a42c123e040df185d8ec6f95cfb98a458577592f
manuel <code@leunam.art>
2023-10-19 18:15:51 +0200

    [feat] added language key

--- database/migrations/2023_10_12_203021_create_posting_table.php
+++ database/migrations/2023_10_12_203021_create_posting_table.php
@@ -10,15 +10,16 @@ return new class extends Migration
     {
         Schema::create('postings', function (Blueprint $table) {
             $table->id();
-            $table->string('project')->nullable();
-            $table->string('site')->nullable();
-            $table->string('element')->nullable();
-            $table->string('title')->nullable();
+            $table->string('project', 100)->nullable();
+            $table->string('site', 100)->nullable();
+            $table->string('element', 100)->nullable();
+            $table->string('title', 255)->nullable();
             $table->longText('body');
             $table->integer('user_id')->unsigned()->index();
             $table->dateTime('onlineFrom')->nullable();
             $table->dateTime('onlineTo')->nullable();
             $table->boolean('active')->nullable()->default(false);
+            $table->string('language', 2)->default('en');
             $table->timestamps();
         });
     }
