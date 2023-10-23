e30e62da54e322c05962cc28964594c2a68e1f1a
manuel <code@leunam.art>
2023-10-23 18:05:23 +0200

    [feat] added return type

--- app/Posting.php
+++ app/Posting.php
@@ -2,11 +2,13 @@
 
 namespace Laravia\Posting\App;
 
+use Laravia\Heart\App\Laravia;
+use Laravia\Posting\App\Models\Posting as ModelsPosting;
 use Laravia\Posting\App\Orchid\Screens\PostingScreen;
 
 class Posting
 {
-    public static function getDashboardMetrics($what)
+    public static function getDashboardMetrics($what): string | false
     {
         try {
             switch ($what) {
