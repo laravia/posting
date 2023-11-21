3020a727eae4650fba0148ab9b096315d3195ec4
manuel <code@leunam.art>
2023-11-21 12:16:31 +0100

    [refactor] removed unneed classes

--- app/Posting.php
+++ app/Posting.php
@@ -2,8 +2,6 @@
 
 namespace Laravia\Posting\App;
 
-use Laravia\Heart\App\Laravia;
-use Laravia\Posting\App\Models\Posting as ModelsPosting;
 use Laravia\Posting\App\Orchid\Screens\PostingScreen;
 
 class Posting
